33000f9aba6bbebc23d1546fb61ffec4f3f80d3c
ahmet-ihsan <ihsanozdemir95@gmail.com>
2024-08-15 22:26:31 +0300

    GPIO - Buton ile 7 Segment Display

---
+++ 0001_GPIO_7SEGMENT/Drivers/Button_Driver.h
@@ -0,0 +1,8 @@
+#ifndef BUTTON_DRIVER_H_
+#define BUTTON_DRIVER_H_
+
+void button_init(void);
+uint8_t button_up(void);
+uint8_t button_down(void);
+
+#endif /* BUTTON_DRIVER_H_ */
---
+++ 0001_GPIO_7SEGMENT/Drivers/Clock_Config_Driver.h
@@ -0,0 +1,6 @@
+#ifndef CLOCK_CONFIG_DRIVER_H_
+#define CLOCK_CONFIG_DRIVER_H_
+
+void rcc_clock_config(void);
+
+#endif /* CLOCK_CONFIG_DRIVER_H_ */
