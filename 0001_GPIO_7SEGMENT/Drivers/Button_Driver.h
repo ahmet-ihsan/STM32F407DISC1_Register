59cd44103a6c5d951a61271f21886b10399a3dca
ahmet-ihsan <79110743+ahmet-ihsan@users.noreply.github.com>
2024-08-16 00:53:06 +0300

    Delete 0001_GPIO_7SEGMENT directory

--- 0001_GPIO_7SEGMENT/Drivers/Button_Driver.h
+++
@@ -1,8 +0,0 @@
-#ifndef BUTTON_DRIVER_H_
-#define BUTTON_DRIVER_H_
-
-void button_init(void);
-uint8_t button_up(void);
-uint8_t button_down(void);
-
-#endif /* BUTTON_DRIVER_H_ */
--- 0001_GPIO_7SEGMENT/Drivers/Clock_Config_Driver.h
+++
@@ -1,6 +0,0 @@
-#ifndef CLOCK_CONFIG_DRIVER_H_
-#define CLOCK_CONFIG_DRIVER_H_
-
-void rcc_clock_config(void);
-
-#endif /* CLOCK_CONFIG_DRIVER_H_ */
